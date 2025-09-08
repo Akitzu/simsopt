ad08d938e5cf5120230ff953f64f2a262a909327
akitzu <ludovic.rais@epfl.ch>
2025-09-08 12:06:30 +0200

    Adding a nurbs curve file and using jaxcurve as parent.

---
+++ src/simsopt/geo/curvenurbs.py
@@ -0,0 +1,14 @@
+from .curve import JaxCurve
+
+__all__ = []
+
+class CurveNURBS(JaxCurve):
+    """
+    A class that implements a NURBS curve.
+    """
+
+    def __init__(self, quadpoints, **kwargs):
+        pass
+
+    def get_dofs(self):
+        pass

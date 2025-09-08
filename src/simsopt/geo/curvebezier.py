18fb6270634ebaf69385a9bafa4120f64c473d7d
akitzu <ludovic.rais@epfl.ch>
2025-09-08 14:16:26 +0200

    change the nurbs to bezier

---
+++ src/simsopt/geo/curvebezier.py
@@ -0,0 +1,13 @@
+from .curve import JaxCurve
+from numpy.typing import ArrayLike
+import jax.numpy as jnp
+
+__all__ = []
+
+class CurveBezier(JaxCurve):
+    """
+    A class that implements a Piecewise Cubic Bezier curve.
+    """
+
+    def __init__(self, points : ArrayLike, order = 1, weights = None):
+        pass

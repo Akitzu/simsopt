e95da53aa03344a548a249b2119e6816609ca0c8
akitzu <ludovic.rais@epfl.ch>
2025-09-23 15:59:32 +0200

    cleaning attributes, removing nn interpolation

--- src/simsopt/geo/curvexyzspline.py
+++ src/simsopt/geo/curvexyzspline.py
@@ -40,8 +40,7 @@ class CurveXYZSpline(JaxCurve):
     "catmull-rom",
     "akima",
     "monotonic",
-    "monotonic-0", 
-    "nearest", 
+    "monotonic-0",
     "linear")
 
     def __init__(self, quadpoints : Union[ArrayLike,int] = None, dofs : DOFs = None, **kwargs):
@@ -103,80 +102,69 @@ class CurveXYZSpline(JaxCurve):
 
     def set_dofs_impl(self, x : ArrayLike):
         """
-        This function sets the dofs associated to this object.
+        There are no local dofs to set as they are all enclosed in the DOFs object.
         """
         pass
-        # n = len(x) // 4
-        # self._knots_size = n
-        # self._arclengths = np.array(x[:n])
-        # self._knots = np.array(x[n:]).reshape((n, 3))
+
+    def fix_arclengths(self):
+        """
+        This function fixes the arclengths dofs, so that only the knots can be modified.
+        """
+        for i in range(self._knots_size):
+            self.fix(f'arclength({i})')
         
-    def plot_knots(self, **kwargs):
-        if 'ax' in kwargs:
-            ax = kwargs.pop('ax')
-            fig = ax.get_figure()
-        else:
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-
-        # Plot the knots as points in 3D space
-        ax.plot(self._knots[:, 0], self._knots[:, 1], self._knots[:, 2], 'o-', label='Knots')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.set_title('Knots in 3D')
-        ax.grid()
-
-        if 'show' not in kwargs or kwargs.pop('show'):
-            plt.show()
-        return ax
-
     # arclengths property : arclengths on the curve at the knots
     @property
     def arclengths(self):
         return self.local_full_x[:self._knots_size]
-    # @arclengths.setter
-    # def arclengths(self, arclengths : ArrayLike):
-    #     if len(arclengths) != self._knots_size:
-    #         raise ValueError("Length of arclengths must match the number of knots")
-    #     elif not all(isinstance(a, float) or isinstance(a, np.floating) for a in arclengths):
-    #         raise ValueError("All elements of arclengths must be floats")
-    #     elif np.any(np.diff(arclengths) <= 0):
-    #         raise ValueError("Arclengths must be in strictly increasing order")
-    #     elif arclengths[0] < 0 or arclengths[-1] >= 1:
-    #         raise ValueError("Arclengths must be in the interval [0, 1)")
-    #     elif self.local_full_dof_size != self.local_dof_size:
-    #         raise ValueError("Cannot set arclengths when  of DOFs has changed. Create a new CurveXYZSpline instance instead.")
+    
+    @arclengths.setter
+    def arclengths(self, arclengths : ArrayLike):
+        if len(arclengths) != self._knots_size:
+            raise ValueError("Length of arclengths must match the number of knots")
+        elif not all(isinstance(a, float) or isinstance(a, np.floating) for a in arclengths):
+            raise ValueError("All elements of arclengths must be floats")
+        elif np.any(np.diff(arclengths) <= 0):
+            raise ValueError("Arclengths must be in strictly increasing order")
+        elif arclengths[0] < 0 or arclengths[-1] >= 1:
+            raise ValueError("Arclengths must be in the interval [0, 1)")
+        elif self.local_full_dof_size != self.local_dof_size:
+            raise ValueError("Cannot set arclengths when  of DOFs has changed. Create a new CurveXYZSpline instance instead.")
 
-    #     # update the DOFS object
-    #     self. = np.hstack((arclengths, self.x[self._knots_size:]))
+        # update the DOFS object
+        self.x = np.hstack((arclengths, self.x[self._knots_size:]))
 
     # knots property : provided points where the curve values are specified 
     @property
     def knots(self):
         return self.local_full_x[self._knots_size:].reshape((self._knots_size, 3))
-    # @knots.setter
-    # def knots(self, knots : ArrayLike):
-    #     if len(knots) != self._knots_size:
-    #         raise ValueError("Length of new knots must match length of old knots")
-    #     elif not all(isinstance(k, (list, np.ndarray)) and len(k) == 3 for k in knots):
-    #         raise ValueError("All elements of knots must be array-like of length 3")
+    
+    @knots.setter
+    def knots(self, knots : ArrayLike):
+        if len(knots) != self._knots_size:
+            raise ValueError("Length of new knots must match length of old knots")
+        elif not all(isinstance(k, (list, np.ndarray)) and len(k) == 3 for k in knots):
+            raise ValueError("All elements of knots must be array-like of length 3")
 
-    #     # update the DOFS object
-    #     self.x = np.hstack((self.x[:self._knots_size], np.array(knots).flatten()))
+        # update the DOFS object
+        self.x = np.hstack((self.x[:self._knots_size], np.array(knots).flatten()))
     
     # method of interpolation
     @property
     def method(self):
         return self._method
+    
     @method.setter
     def method(self, method : str):
         if method not in self.INTERPAX_METHODS_1D:
             raise ValueError(f"Method must be one of {self.INTERPAX_METHODS_1D}")
         self._method = method
-    
+   
     @staticmethod
     def _make_names(nknots : PositiveInteger):
+        """
+        Static method to create names for the dofs of the CurveXYZSpline object.
+        """
         phi_names = [f'arclength({i})' for i in range(nknots)]
         x_names = [f'x({i})' for i in range(nknots)]
         y_names = [f'y({i})' for i in range(nknots)]
@@ -193,3 +181,26 @@ class CurveXYZSpline(JaxCurve):
             arclenghts = np.linspace(0, 1, len(knots), endpoint=False)
 
         return cls(quadpoints=quadpoints, x0=np.hstack((arclenghts, np.array(knots).flatten())), method=method)
+    
+    def plot_knots(self, **kwargs):
+        """
+        Plots the knots (known points) used to  in 3D space.
+        """
+        if 'ax' in kwargs:
+            ax = kwargs.pop('ax')
+            fig = ax.get_figure()
+        else:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+
+        # Plot the knots as points in 3D space
+        knots = self.knots
+        ax.plot(*knots.T, 'o', label='Knots')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.grid()
+
+        if 'show' not in kwargs or kwargs.pop('show'):
+            plt.show()
+        return ax
